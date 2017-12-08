afb44a265b1ad2272c253f7223bd880cbf1e49ad
Michał Krakowiak <rubikshift@users.noreply.github.com>
2017-12-08 10:45:29 +0100

    insert, select

---
+++ select.sql
@@ -0,0 +1,23 @@
+SELECT *
+	FROM Autorzy
+
+SELECT *
+	FROM Ksiazki
+
+SELECT *
+	FROM Napisal
+
+SELECT *
+	FROM Promocje
+
+SELECT *
+	FROM Przecenione
+
+SELECT *
+	FROM Uzytkownicy
+
+SELECT *
+	FROM DaneDostawy
+
+SELECT *
+	FROM Zamowienia
