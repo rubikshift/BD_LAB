9bded6c5a401c2df28a5cbb92dd075e809d49119
Michał Krakowiak <michal.krak97@gmail.com>
2017-12-08 09:15:33 +0100

    not null

--- create.sql
+++ create.sql
@@ -14,59 +14,63 @@ CREATE TABLE Ksiazki (
 
 CREATE TABLE Napisal (
 	IDAutora	INT REFERENCES Autorzy NOT NULL,
-	ISBNKsiazki	CHAR(13) REFERENCES Ksiazki,
+	ISBNKsiazki	CHAR(13) REFERENCES Ksiazki NOT NULL,
 
 	PRIMARY KEY (IDAutora, ISBNKsiazki)
 )
 
 CREATE TABLE Promocje (
 	ID				INT	PRIMARY KEY,
-	Nazwa			VARCHAR(50),
-	DataRozpoczecia	DATE,
-	DataZakonczenia	DATE,
-	Obnizka			INT
+	Nazwa			VARCHAR(50) NOT NULL,
+	DataRozpoczecia	DATE NOT NULL,
+	DataZakonczenia	DATE NOT NULL,
+	Obnizka			INT NOT NULL
 )
 
 CREATE TABLE Przecenione (
-	IDPromocji	INT REFERENCES Promocje,
-	ISBNKsiazki	CHAR(13) REFERENCES Ksiazki,
+	IDPromocji	INT REFERENCES Promocje NOT NULL,
+	ISBNKsiazki	CHAR(13) REFERENCES Ksiazki NOT NULL,
 
 	PRIMARY KEY (IDPromocji, ISBNKsiazki)
 )
 
 CREATE TABLE Uzytkownicy (
 	ID			INT PRIMARY KEY,
-	Email		VARCHAR(50),
-	PassHash	CHAR(256)
+	Email		VARCHAR(50) NOT NULL,
+	PassHash	CHAR(256) NOT NULL
 )
 
 CREATE TABLE DaneDostawy (
 	ID					INT PRIMARY KEY,
-	ImieOdbiorcy		VARCHAR(30),
-	NazwiskoOdbiorcy	VARCHAR(50),
-	AdresDostawy		VARCHAR(100),
-	NrTelefonu			CHAR(9),
-	IDUzytkownika		INT REFERENCES Uzytkownicy
+	ImieOdbiorcy		VARCHAR(30) NOT NULL,
+	NazwiskoOdbiorcy	VARCHAR(50) NOT NULL,
+	AdresDostawy		VARCHAR(100) NOT NULL,
+	NrTelefonu			CHAR(9) NOT NULL,
+	IDUzytkownika		INT REFERENCES Uzytkownicy NOT NULL
 )
 
 CREATE TABLE Zamowienia (
 	NrZamowienia		INT PRIMARY KEY,
-	DataZlozenia		DATE,
-	Wartosc				SMALLMONEY,
-	SposobDostawy		INT REFERENCES DaneDostawy,
+	DataZlozenia		DATE NOT NULL,
+	Wartosc				SMALLMONEY NOT NULL,
+	SposobDostawy		INT REFERENCES DaneDostawy NOT NULL,
 	DataWyslania		DATE,
 	DataZrealizowania	DATE
 )
 CREATE TABLE PozycjeZamowienia (
-	NrZamowienia	INT REFERENCES Zamowienia,
-	ISBNKsiazki		CHAR(13) REFERENCES Ksiazki,
+	NrZamowienia	INT REFERENCES Zamowienia NOT NULL,
+	ISBNKsiazki		CHAR(13) REFERENCES Ksiazki NOT NULL,
 
 	PRIMARY KEY	(NrZamowienia, ISBNKsiazki)
 )
 
 CREATE TABLE WirtualnePolki (
-	IDUzytkownika	INT REFERENCES Uzytkownicy,
-	Nazwa			VARCHAR(50),
+	IDUzytkownika	INT REFERENCES Uzytkownicy NOT NULL,
+	Nazwa			VARCHAR(50) NOT NULL,
 
 	PRIMARY KEY (IDUzytkownika, Nazwa)
+)
+
+CREATE TABLE OdlozoneNaPolke (
+	Polka	FOREIGN KEY REFERENCES WirtualnePolki
 )
